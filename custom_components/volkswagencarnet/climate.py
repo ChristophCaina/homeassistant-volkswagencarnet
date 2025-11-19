0b615d1ef73c8413045f631e825305b9f294d752
Christoph <Christoph@Caina.de>
2025-11-19 13:17:17 +0100

    Add async_setup_platform for Volkswagen climate

--- custom_components/volkswagencarnet/climate.py
+++ custom_components/volkswagencarnet/climate.py
@@ -10,3 +10,11 @@ from . import VolkswagenEntity
 from .const import DATA, DATA_KEY, DOMAIN, UPDATE_CALLBACK
 
 _LOGGER = logging.getLogger(__name__)
+
+async def async_setup_platform(
+    hass: HomeAssistant, config, async_add_entities, discovery_info=None
+):
+    """Set up the Volkswagen climate platform."""
+    if discovery_info is None:
+        return
+    async_add_entities([VolkswagenClimate(hass.data[DATA_KEY], *discovery_info)])
