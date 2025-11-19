e87c360a888643444bf1eded75662fe9540bc98c
Christoph <Christoph@Caina.de>
2025-11-19 13:15:33 +0100

    Add docstring and imports to climate.py
    Added module docstring and import statements.

--- custom_components/volkswagencarnet/climate.py
+++ custom_components/volkswagencarnet/climate.py
@@ -1,1 +1,12 @@
+"""Climate support for Volkswagen Connect integration."""
+
+import logging
+
 from homeassistant.components.climate import ClimateEntity, HVACMode, ClimateEntityFeature
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity import EntityCategory
+
+from . import VolkswagenEntity
+from .const import DATA, DATA_KEY, DOMAIN, UPDATE_CALLBACK
+
+_LOGGER = logging.getLogger(__name__)
